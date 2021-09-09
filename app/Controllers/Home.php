e0b733a3a069130eac109cfb8e7509578129e515
JiHoon <eejjhh222@naver.com>
2021-09-10 00:01:21 +0900

    upgrade

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,11 +6,32 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		echo "HI";
+//		return view('welcome_message');
+	}
+
+	function round_down($num, $d = 0)
+	{
+		return $this->sgn($num)*$this->p_floor(abs($num), $d);
+	}
+
+
+
+	function p_floor( $val, $d )
+	{
+		return floor($val * pow (10, $d) )/ pow (10, $d) ;
+	}
+
+
+	function sgn($x)
+	{
+		return $x ? ($x>0 ? 1 : -1) : 0;
 	}
 
 	public function testPage()
 	{
+		$ff = floor(17.4/pow(10,-2));
+		echo $this->round_down(17.40,2);
 		return view('test_page');
 	}
 
